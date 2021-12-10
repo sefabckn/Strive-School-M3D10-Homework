ffa12d21aec4bcb7a134caca1ae9aac81fc6cf65
SefaBckn <31974256+sefabckn@users.noreply.github.com>
2021-12-10 15:22:35 +0100

    movie posting is working

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
-const myToken = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTgyOTU0M2FhY2FhMjAwMTU1MmExOWYiLCJpYXQiOjE2MzYxMjEzNDMsImV4cCI6MTYzNzMzMDk0M30.zFcoolIgUcEP9_uinWyWKWl-LAtFg3vMUZ07CpVc2Hw"
+const myToken = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTgyOTU0M2FhY2FhMjAwMTU1MmExOWYiLCJpYXQiOjE2MzkxNDMwNjEsImV4cCI6MTY0MDM1MjY2MX0.P8swnCNNDtgTs6OrEXKqzZ-rjLq9uRMb8lxQfa87uI8"
 const url = "https://striveschool-api.herokuapp.com/api/movies/"    
-
-
-
-
-
-
+const genreUrl = 'https://striveschool-api.herokuapp.com/api/movies/'
 
 const getMovies = async() =>{
     try{
         const response = await fetch(url,{
-            method:"GET",
           headers:{
             'Content-Type': 'application/json',
             "Authorization": myToken
@@ -23,7 +17,60 @@ const getMovies = async() =>{
         console.log(error)
     }
 }
+const getMovieByGenre = async() =>{
+    try{
+        const response = await fetch(genreUrl,{
+          headers:{
+            'Content-Type': 'application/json',
+            "Authorization": myToken
+          }  
+        })
+        if(!response.ok) throw new Error("Failed to Fetch Movies")
+        const moviesByGenre = await response.json()
+        console.log(moviesByGenre)
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+const saveBtn = document.querySelector('#save-btn')
+
+const postMovie = async(event) =>{
+    event.preventDefault()
+    try{
+        const inputValues = {
+            name: document.querySelector('#name').value,
+            description: document.querySelector('#description').value,
+            category: document.querySelector('#category').value,
+            imageUrl: document.querySelector('#imageUrl').value,
+        }
+        console.log(inputValues)
+        const response = await fetch(genreUrl,{
+            method: 'POST',
+            body: JSON.stringify(inputValues),
+            headers:{
+            "Authorization": myToken,
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          }  
+        })
+        if(!response.ok) throw new Error("Failed to Fetch Movies")
+        const postData = await response.json()
+        console.log(postData)
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+
+const validateForm = (event) => {
+    event.target.form.classList.add('validated')
+}
+
 window.onload = ()=>{
     getMovies()
+    getMovieByGenre()
+    postMovie()
     
 }
+
